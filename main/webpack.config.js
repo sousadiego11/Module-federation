15f7288a5213db12fed017cb9e8a53281f8ba9cf
Diego Sousa <diegointeriores11@gmail.com>
2022-06-25 19:02:16 -0300

    refactor: wp5 config

--- main/webpack.config.js
+++ main/webpack.config.js
@@ -33,5 +33,5 @@ module.exports = {
             }
         ]
     },
-    plugins: [ new HtmlWebpackPlugin({ template: './src/public/index.html' }) ]
+    plugins: [ new HtmlWebpackPlugin({ template: './src/public/index.html', inject: true }) ]
 }
